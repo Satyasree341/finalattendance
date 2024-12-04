f29ce589f265d0156587277abc01e58301010610
prasadpathivada <155958450+prasadpathivada@users.noreply.github.com>
2024-12-04 16:45:52 +0530

    Update AttendanceRepository.java

--- attendence/src/main/java/com/example/attendance/repository/AttendanceRepository.java
+++ attendence/src/main/java/com/example/attendance/repository/AttendanceRepository.java
@@ -10,6 +10,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AttendanceRepository extends JpaRepository<AttendanceData, Long> {
 
-    boolean existsByUserAndLoginOptionAndLoginTimeAfter(User user, String loginOption, Date startOfDay);
-    List<AttendanceData> findByUserId(Long userId);
+    // boolean existsByUserAndLoginOptionAndLoginTimeAfter(User user, String loginOption, Date startOfDay);
+    // List<AttendanceData> findByUserId(Long userId);
+
+    // DP UPDATE BELOW CODE 
+ @Query("SELECT a FROM AttendanceData a WHERE a.user.id = :userId " +
+                        "AND DATE(a.loginTime) = :date ORDER BY a.loginTime DESC limit 1")
+        AttendanceData findTopByUserIdAndLoginDateOrderByLoginTimeDesc(
+                        @Param("userId") Integer userId,
+
+                        @Param("date") LocalDate date);
+
+        List<AttendanceData> findByUserId(Long id);
+
+        boolean existsByUserAndLoginOptionAndLoginTimeAfterAndInstituteId(User user, String loginOption,
+                        Date startOfDay,
+                        Long instituteId);
+}
+    
 }
