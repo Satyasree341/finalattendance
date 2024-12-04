257de6b117280dd003d62fb0af77bcae71565063
prasadpathivada <155958450+prasadpathivada@users.noreply.github.com>
2024-12-04 16:50:01 +0530

    Update InstituteRepository.java

--- attendence/src/main/java/com/example/attendance/repository/InstituteRepository.java
+++ attendence/src/main/java/com/example/attendance/repository/InstituteRepository.java
@@ -1,1 +1,9 @@
+package com.example.attendance.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.attendance.model.Institute;
+
+public interface InstituteRepository extends JpaRepository<Institute, Long> {
+    Institute findByInstituteName(String instituteName);
+}
